0b705d3bb849978aaa4fbd4fd6e8e6d596835f6d
FlowGoCrazy <65233548+FlowGoCrazy@users.noreply.github.com>
2021-01-30 20:26:52 -0500

    Handle Capital Letters
    I've been using this library in the production of every single project I've worked on since it was released.
    Unfortunately I've found A bug that must be resolved as soon as possible as it affects the system adversely and renders it unusable until it is resolved.
    If isMonkey is called with Monkey, mOnkey, monkeY, etc it currently incorrectly returns that the val passed is not monkey even though it is but with some capitalization.
    I've added a fix to change all characters in val to lowercase before comparing val to 'monkey'.
    Please merge this at your earliest convenience, this bug is currently causing my projects in production millions! 😢

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
 const isMonkey = function(val) {
-  return (val === 'monkey'); //returns if string is 'monkey' or not
+  return (val.toLowerCase() === 'monkey'); //returns if string is 'monkey' or not
 };
 
 module.exports = isMonkey;
